20c2190d4581009963f230a6befe03c82ee56db6
Jukka Svahn <web@rahinaa.biz>
2012-04-02 20:37:39 +0300

    Use a pluggable_ui instead of JavaScript. Compatibility with Textpattern r3650 and newer.

--- rah_status_dropdown.php
+++ rah_status_dropdown.php
@@ -13,37 +13,21 @@
  * http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-	if(@txpinterface == 'admin')
-		register_callback('rah_status_dropdown', 'admin_side', 'head_end');
+	if(@txpinterface == 'admin') {
+		register_callback('rah_status_dropdown', 'article_ui', 'status');
+	}
 
 /**
- * Adds JavaScript to the <head>
+ * Replace status radios with a drop down
  */
 
-	function rah_status_dropdown() {
-		global $event;
-
-		if($event != 'article')
-			return;
-
-		$js = <<<EOF
-			$(document).ready(function() {
-				$('#write-status').append('<p class="status rah_status_dropdown"><select id="rah_status_dropdown" name="Status" class="list"></select></p>');
-				var to = $('#rah_status_dropdown');
-				$('#write-status li').each(
-					function() {
-						var input = $(this).children('input[type=radio]');
-						to.append(
-							'<option value="'+input.val()+'"'+
-								(input.is(':checked') ? ' selected="selected"' : '' )+
-							'>' + $(this).children('label').text()+'</option>'
-						);
-					}
-				);
-				$('#write-status ul').remove();
-			});
-EOF;
-
-		echo script_js($js);
+	function rah_status_dropdown($event, $step, $pre, $rs) {
+		global $statuses;
+		
+		return 
+			'<fieldset id="write-status">'.n.
+				'<legend>'.gTxt('status').'</legend>'.n.
+				graf(selectInput('Status', doArray($statuses, 'strip_tags'), !$rs['Status'] ? 4 : $rs['Status'])).
+			'</fieldset>'.n;
 	}
 ?>
